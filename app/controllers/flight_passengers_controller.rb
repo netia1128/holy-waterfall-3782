6c0339f7ae892ad71d0c58b045e3959af9136468
Netia Ingram <76889420+netia1128@users.noreply.github.com>
2021-06-11 10:58:53 -0600

    Remove uneessary instance variables

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -3,7 +3,7 @@ class FlightPassengersController < ApplicationController
   def destroy
     flight = Flight.find(params[:flight_id])
     passenger = Passenger.find(params[:id])
-    flight_passenger = FlightPassenger.find_by(flight: @flight, passenger: @passenger)
+    flight_passenger = FlightPassenger.find_by(flight: flight, passenger: passenger)
     flight_passenger.destroy
     redirect_to flights_path
   end
